1668270e114a5b3777fd590d4af42875b85c5d76
Malachi Burke <malachi.burke@gmail.com>
2016-02-04 15:25:24 -0800

    Moving Console code in from external project

---
+++ src/Console.cpp
@@ -0,0 +1,57 @@
+#include "Console.h"
+
+void Console::handler()
+{
+  while(cin.available() > 0)
+  {
+    char received = cin.read();
+    cout << received;
+
+    // newline received, actually perform action
+    if(received == '\n' || received == 13)
+    {
+      int paramCounter = 0;
+      char* parameters[10];
+
+      inputLine[inputPos] = 0;
+
+      // No input = just show prompt again
+      if(inputPos == 0)
+      {
+        cout.println();
+        showPrompt();
+        return;
+      }
+
+      //Serial << F("Submitting command: ") << inputLine << F("\n");
+
+      parameters[0] = inputLine;
+
+      for(int i = 0; i < inputPos; i++)
+      {
+        if(inputLine[i] == ' ')
+        {
+          inputLine[i] = 0;
+          //Serial << F("param# ") << paramCounter << F(" = ") << parameters[paramCounter];
+          paramCounter++;
+          // FIX: preload code is kludgey, replace with something better (wastes one slot)
+          // if we can sneak ahead one character safely, preload next paramCounter
+          if(i + 1 < inputPos)
+          {
+            parameters[paramCounter] = &inputLine[i + 1];
+          }
+        }
+      }
+
+      handleCommand(Parameters(parameters, paramCounter + 1));
+
+      cout.println();
+
+      showPrompt();
+
+      inputPos = 0;
+    }
+    else
+      inputLine[inputPos++] = received;
+  }
+}
---
+++ src/Console.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "lib.h"
+
+class IMenuHandler : public util::IHandler
+{
+
+};
+
+
+
+class IMenu
+{
+public:
+  struct Parameters
+  {
+    char** parameters;
+    uint8_t count;
+
+    Parameters(char** parameters, int count) :
+      parameters(parameters), count(count) {}
+
+    Parameters inc()
+    {
+      Parameters p(parameters + 1, count - 1);
+
+      return p;
+    }
+  };
+
+protected:
+  virtual void showPrompt() = 0;
+  virtual void handleCommand(Parameters p) = 0;
+};
+
+class Menu : public IMenu
+{
+
+};
+
+
+// Glues I/O logic to menu (interacts with Serial, etc)
+class Console : public IMenu
+{
+  char inputLine[128];
+  uint8_t inputPos = 0;
+  //Print& cout; // TODO
+  //Print& cin;
+
+public:
+
+protected:
+  void showMenu(Parameters p);
+
+public:
+  void handler();
+  bool handler(char** parameters, int count, PGM_P keyword, void (Console::*func)(void));
+};
+
+// temporary until we fully enable this inside Console class itself
+#ifdef SAML_SERIES
+#define cout SerialUSB
+#define cin SerialUSB
+#else
+#define cout Serial
+#define cin Serial
+#endif
