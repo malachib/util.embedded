20a3257041f85d0a1d2e8d7b7767d3818a9b35ac
Malachi Burke <malachi.burke@gmail.com>
2016-02-06 08:59:28 -0800

    Code organization

--- src/Console.cpp
+++ src/Console.cpp
@@ -1,11 +1,5 @@
 #include "Console.h"
 
-#ifndef AVR
-// samd is supposed to have a strncmp_P mapping to regular strncmp,
-// but doesn't for some reason (probably platformio outdated) so do it here
-#define strncmp_P(s1, s2, n) strncmp((s1), (s2), (n))
-#endif
-
 using namespace FactUtilEmbedded;
 
 
@@ -74,58 +68,6 @@ void Console::handler()
   }
 }
 
-Menu* MenuHandler::canHandle(Parameters p)
-{
-  return MenuEnumerator::canHandle(p);
-}
-
-void MenuHandler::handleCommand(Parameters p)
-{
-  Menu* menu = canHandle(p);
-  if(menu != NULL)
-    menu->handleCommand(p.inc());
-  else if(strcmp_P(*p.parameters, PSTR("help")) == 0)
-  {
-    showHelp(p.inc());
-  }
-  else
-  {
-    cout.println();
-    cout << F("Unrecognized command: ") << *p.parameters;
-    cout.println();
-  }
-}
-
-
-void MenuHandler::showHelp(Parameters p)
-{
-  if(p.count == 0)
-  {
-    SinglyLinkedNode* node = getHeadMenu();
-
-    cout.println();
-
-    if(node == NULL)
-    {
-      cout << F("No commands registered");
-      return;
-    }
-
-    cout << F("Following commands are available: ");
-
-    cout.println();
-    cout.println();
-
-    for(; node != NULL; node = node->getNext())
-    {
-      Menu* menu = (Menu*) node;
-
-      cout << F("  ");
-      showKeyValuePair(menu->getName(), menu->getDescription(), 16);
-      cout.println();
-    }
-  }
-}
 
 void ConsoleMenuHandler::handleCommand(Parameters p)
 {
@@ -150,40 +92,7 @@ bool ConsoleMenuHandler::processInput(Console* console, char received)
 {
   return getActiveMenu()->processInput(console, received);
 }
-
-
-bool MenuHandler::processInput(Console* console, char received)
-{
-  // look for tab character
-  if(received == 9)
-  {
-    char* inputLine = console->getInputLine();
-    uint8_t inputPos = console->getInputPos();
-
-    SinglyLinkedNode* node = getHeadMenu();
-    for(; node != NULL; node = node->getNext())
-    {
-      Menu* menu = (Menu*) node;
-      const char* commandName = (const char*) menu->getName();
-      if(strncmp_P(inputLine, commandName, inputPos) == 0)
-      {
-        // TODO: make an appendToInputLine_P
-        char temp[32];
-
-        strcpy_P(temp, commandName + inputPos);
-
-        cout << temp;
-
-        console->appendToInputLine(temp);
-      }
-    }
-    return true;
-  }
-  else
-    return false;
-}
 #endif
-
 
 void MenuEnumerator::add(Menu& menu)
 {
@@ -209,36 +118,4 @@ Menu* MenuEnumerator::canHandle(IMenu::Parameters p)
   }
 
   return NULL;
-}
-
-
-void Menu::showPrompt()
-{
-  cout << name;
-}
-
-IMenu* Menu::canHandle(IMenu::Parameters p)
-{
-  if(strcmp_P(p.parameters[0], (const char*) name) == 0)
-    return this;
-  else
-    return NULL;
-}
-
-void MenuGeneric::handleCommand(IMenu::Parameters p)
-{
-  handler(p);
-}
-
-void IMenu::showKeyValuePair(const __FlashStringHelper* key, const __FlashStringHelper* value, uint8_t keyPadding)
-{
-  // FIX: some Print classes don't seem to return proper bytes-written
-  size_t nameLength = strlen_P((const char*) key);
-  //size_t nameLength = cout.print(menu->getName());
-  cout.print(key);
-  if(keyPadding > nameLength)
-    keyPadding -= nameLength;
-
-  while(keyPadding-- > 0) cout.print(' ');
-  cout.print(value);
 }
--- src/Console.h
+++ src/Console.h
@@ -1,13 +1,15 @@
 #pragma once
 
-#include "lib.h"
-#include <LinkedList.h>
-
-
 #define CONSOLE_FEATURE_AUTOCOMPLETE 1
 #define CONSOLE_FEATURE_ENHANCED_CHARPROCESSOR 1
 //#define CONSOLE_FEATURE_MULTICONSOLE
 //#define CONSOLE_BEHAVIOR_PROPAGATE
+
+
+#include "lib.h"
+#include "Menu.h"
+#include <LinkedList.h>
+
 
 namespace FactUtilEmbedded
 {
@@ -23,60 +25,6 @@ class IEnterable
 protected:
   virtual void enter() = 0;
   virtual void exit() = 0;
-};
-
-class ConsoleMenuHandler;
-class Console;
-class MenuHandler;
-
-class IMenuBase
-{
-public:
-  struct Parameters
-  {
-    char** parameters;
-    uint8_t count;
-
-#ifdef CONSOLE_BEHAVIOR_PROPAGATE
-    Console* console;
-#endif
-
-    Parameters(char** parameters, int count, Console* console) :
-      parameters(parameters), count(count)
-#ifdef CONSOLE_BEHAVIOR_PROPAGATE
-      ,console(console)
-#endif
-      {}
-
-    Parameters inc()
-    {
-#ifdef CONSOLE_BEHAVIOR_PROPAGATE
-      Parameters p(parameters + 1, count - 1, console);
-#else
-      Parameters p(parameters + 1, count - 1, NULL);
-#endif
-
-      return p;
-    }
-  };
-};
-
-class IMenu : public IMenuBase
-{
-  friend ConsoleMenuHandler;
-  friend MenuHandler;
-
-protected:
-  virtual void showPrompt() = 0;
-  virtual void handleCommand(Parameters p) = 0;
-#ifdef CONSOLE_FEATURE_ENHANCED_CHARPROCESSOR
-  // return value of true means input was processed and needs no further processing.
-  // note that process does not mean command executed, but only that the one character
-  // was handled in a specific way (i.e. tab completion)
-  virtual bool processInput(Console* console, char c) { return false; }
-#endif
-
-  static void showKeyValuePair(const __FlashStringHelper* key, const __FlashStringHelper* value, uint8_t keyPadding);
 };
 
 // Glues I/O logic to menu (interacts with Serial, etc)
