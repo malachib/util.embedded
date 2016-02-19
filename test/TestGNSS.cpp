85da9020aae7a8f3dae45ad7a311f5af6aa908c8
Malachi Burke <malachi.burke@gmail.com>
2016-02-19 03:22:44 -0800

    GPS parsing appears to be working great.  But not in the mmu...

--- test/TestGNSS.cpp
+++ test/TestGNSS.cpp
@@ -9,6 +9,8 @@ using namespace driver;
 
 class Dummy_GNSS_raw : public IGNSS_raw
 {
+  char* inputBuffer;
+
 public:
   VIRTUAL MetaData getMetaData() OVERRIDE
   {
@@ -17,6 +19,8 @@ public:
   }
   VIRTUAL uint16_t getContextSize() OVERRIDE { return 0; }
   VIRTUAL bool getGNSS(token_callback callback, void* context = NULL) OVERRIDE;
+
+  void setInput(char* inputBuffer) { this->inputBuffer = inputBuffer; }
 };
 
 
@@ -59,21 +63,37 @@ bool getGNSS_helper(IGNSS_raw::token_callback callback, char* input, char* buffe
 
 }
 
-GnssFields fieldOrder[] =
+GnssFields testFieldOrder[] =
 {
   GNSS_LATITUDE, GNSS_LONGITUDE, GNSS_SPEED, GNSS_HEADING, GNSS_TIMESTAMP,
   // skip altitude (#5) to test for false positives
   GNSS_LATP, GNSS_LONGP
 };
 
+GnssFields //PROGMEM
+SIM808_fieldOrder[] =
+{
+  GNSS_STATUS_RUN,
+  GNSS_STATUS_FIX,
+  GNSS_TIMESTAMP,
+  GNSS_LATITUDE,
+  GNSS_LONGITUDE,
+  GNSS_ALTITUDE,
+  GNSS_SPEED,
+  GNSS_HEADING
+};
+
+
 #define INPUT_BUFFER "1,2,12.0,14.0,XZ,,ABC"
 char inputBuffer[] = INPUT_BUFFER;
 
+#define INPUT_BUFFER2 "+CGNSINF: 1,1,20160219110053.000,34.098948,-118.138393,166.100,0.93,93.4,1,,0.8,1.1,0.8,,12,11,,,39,,"
+char inputBuffer2[] = INPUT_BUFFER2;
 
 bool Dummy_GNSS_raw::getGNSS(IGNSS_raw::token_callback callback, void* context)
 {
-  return getGNSS_helper_d(callback, inputBuffer, fieldOrder,
-    sizeof(fieldOrder) / sizeof(GnssFields), context );
+  return getGNSS_helper_d(callback, inputBuffer, testFieldOrder,
+    sizeof(testFieldOrder) / sizeof(GnssFields), context );
   TokenizerInPlace tokenizer(inputBuffer,",");
   for(int i = 0; i < 5; i++)
   {
@@ -143,12 +163,44 @@ bool token_callback(GnssFields field, char* _token, void* context)
   return true;
 }
 
+
+bool token_callback2(GnssFields field, char* _token, void* context)
+{
+  std::string token = _token;
+
+  switch(field)
+  {
+    case GNSS_STATUS_RUN:
+      printf("\r\nStatus run %s", _token);
+      break;
+
+    case GNSS_STATUS_FIX:
+      printf("\r\nStatus fix %s", _token);
+      REQUIRE(token == "1");
+      break;
+
+    case GNSS_LATITUDE:
+      printf("\r\nLatitude: %s", _token);
+      REQUIRE(token == "34.098948");
+      break;
+
+    case GNSS_LONGITUDE:
+      printf("\r\nLongitude: %s", _token);
+      REQUIRE(token == "-118.138393");
+      break;
+
+  }
+  return true;
+}
+
 SCENARIO( "GNSS synthetic tests", "[gnss]" )
 {
   Dummy_GNSS_raw gnss_raw;
 
   GIVEN("A given GPS input of '1,2,12.0,14.0,XZ,,ABC'")
   {
+    gnss_raw.setInput(inputBuffer);
+
     bool fulfilled[] = { false, false, false, false, false, false, false, false };
 
     Context context;
@@ -160,8 +212,8 @@ SCENARIO( "GNSS synthetic tests", "[gnss]" )
     {
       char tempBuffer[20];
 
-      getGNSS_helper(token_callback, inputBuffer, tempBuffer, fieldOrder,
-        sizeof(fieldOrder) / sizeof(GnssFields), &context);
+      getGNSS_helper(token_callback, inputBuffer, tempBuffer, testFieldOrder,
+        sizeof(testFieldOrder) / sizeof(GnssFields), &context);
 
       for(int i = 0; i < 7; i++)
       {
@@ -188,5 +240,21 @@ SCENARIO( "GNSS synthetic tests", "[gnss]" )
     // doesn't work since inputBuffer has indeed been changed
     //REQUIRE(std::string(inputBuffer) == INPUT_BUFFER);
     //REQUIRE(std::string(context.inputBuffer) == INPUT_BUFFER);
+  }
+  GIVEN("A GPS input of: " INPUT_BUFFER2)
+  {
+    char *p = strstr(inputBuffer2, F("SINF"));
+
+    REQUIRE(p);
+
+    p+=6;
+
+    //gnss_raw.setInput(p);
+
+    INFO("modified input buffer = " << p);
+
+    getGNSS_helper_d(token_callback2, p, SIM808_fieldOrder,
+      sizeof(SIM808_fieldOrder) / sizeof(GnssFields), NULL );
+    //gnss_raw.getGNSS(token_callback2, NULL);
   }
 }
