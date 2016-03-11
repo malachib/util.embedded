8ab967fcda9b45c4791e9d75140493f080c327f4
Malachi Burke <malachi.burke@gmail.com>
2016-03-10 17:56:36 -0800

    Adding void-return version of invoke

--- src/fact/MenuFunction.h
+++ src/fact/MenuFunction.h
@@ -13,6 +13,13 @@ namespace FactUtilEmbedded
     return result;
   }
 
+  template <class TIn>
+  void invoke(void (*invokee)(TIn), IMenu::Parameters p)
+  {
+    TIn in1 = fromString<TIn>(p.parameters[0]);
+    invokee(in1);
+  }
+
   template <class TIn1, class TIn2>
   int invoke(int (*invokee)(TIn1, TIn2), IMenu::Parameters p)
   {
@@ -20,6 +27,14 @@ namespace FactUtilEmbedded
     auto in2 = fromString<TIn2>(p.parameters[1]);
     int result = invokee(in1, in2);
     return result;
+  }
+
+  template <class TIn1, class TIn2>
+  void invoke(void (*invokee)(TIn1, TIn2), IMenu::Parameters p)
+  {
+    auto in1 = fromString<TIn1>(p.parameters[0]);
+    auto in2 = fromString<TIn2>(p.parameters[1]);
+    invokee(in1, in2);
   }
 
   class MenuFunctionBase : public MenuCommand
--- src/fact/string_convert.h
+++ src/fact/string_convert.h
@@ -1,16 +1,23 @@
+#pragma once
+
 template<class T> T fromString(const char* input);
 
-template<> int fromString(const char* input)
+template<> inline unsigned char fromString(const char* input)
+{
+  return (unsigned char) atoi(input);
+}
+
+template<> inline int fromString(const char* input)
 {
   return atoi(input);
 }
 
-template<> float fromString(const char* input)
+template<> inline float fromString(const char* input)
 {
   return atof(input);
 }
 
-template<> const char* fromString(const char* input)
+template<> inline const char* fromString(const char* input)
 {
   return input;
 }
