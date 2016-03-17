396253445660fcc832e169e64c486007bd2a60e8
Malachi Burke <malachi.burke@gmail.com>
2016-03-17 12:10:45 -0700

    Adding unsigned long fromString converter

--- src/fact/string_convert.h
+++ src/fact/string_convert.h
@@ -17,6 +17,11 @@ template<> inline float fromString(const char* input)
   return atof(input);
 }
 
+template<> inline unsigned long fromString(const char* input)
+{
+  return strtoul(input, NULL, 10);
+}
+
 template<> inline const char* fromString(const char* input)
 {
   return input;
