f31c5a32262028dfb1bdb12204c0a985cebd1a91
Malachi Burke <malachi.burke@gmail.com>
2016-03-21 10:41:54 -0700

    Fiddling with toString helper

--- src/fact/string_convert.h
+++ src/fact/string_convert.h
@@ -4,11 +4,13 @@ template<class T> T fromString(const char* input);
 
 template<> inline unsigned char fromString(const char* input)
 {
+  // TODO: use strtol and friends if available, atoi is deprecated
   return (unsigned char) atoi(input);
 }
 
 template<> inline int fromString(const char* input)
 {
+  // TODO: use strtol and friends if available, atoi is deprecated
   return atoi(input);
 }
 
@@ -31,3 +33,34 @@ template<> inline char fromString(const char* input)
 {
   return input[0];
 }
+
+template<class T> char* toString(char* output, T input);
+
+template<> char* toString(char* output, char input)
+{
+  output[0] = input;
+  output[1] = 0;
+  return output;
+}
+
+
+#ifdef __AVR__
+#include <stdlib.h>
+
+template<> char* toString(char* output, int input)
+{
+  return itoa(input, output, 10);
+}
+
+
+template<> char* toString(char* output, unsigned int input)
+{
+  return utoa(input, output, 10);
+}
+#else
+template<> char* toString(char* output, int input)
+{
+  sprintf(output, "%d", input);
+  return output;
+}
+#endif
