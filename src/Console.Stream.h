0bedfa1a06dcb1215d1bacc3529b8bfad1be6546
Malachi Burke <malachi.burke@gmail.com>
2016-03-27 22:04:28 -0700

    attiny compatibility

--- src/Console.Stream.h
+++ src/Console.Stream.h
@@ -8,7 +8,7 @@
 #ifdef SAML_SERIES
 #define cout SerialUSB
 #define cin SerialUSB
-#else
+#elif not defined(__AVR_ATtiny85__)
 #define cout Serial
 #define cin Serial
 #endif
@@ -20,4 +20,20 @@
 #include <SoftwareSerial.h>
 #endif
 extern CONSOLE_STREAM_EXTERN cout;
+#endif
+
+// NOTE: kinda kludgey, this discovers if no cout exists
+// and if so makes a dummy one so that at least the menu
+// stuff will compile (and be optimized out)
+#if !defined(cout) and !defined(CONSOLE_STREAM_EXTERN)
+class DummyStream : public Print
+{
+public:
+  virtual size_t write(uint8_t) override { return 1; }
+  uint8_t available() { return 0; }
+  char read() { return ' '; }
+} _dummyStream;
+
+#define cin _dummyStream
+#define cout _dummyStream
 #endif
