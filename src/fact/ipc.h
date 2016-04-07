cc0d8b404b4d7c29154d3fa9fcb758fb5839f640
Malachi Burke <malachi.burke@gmail.com>
2016-04-07 12:43:45 -0700

    More experimentation - so far so good

--- src/fact/ipc.h
+++ src/fact/ipc.h
@@ -1,8 +1,20 @@
+
+class ParameterClass_0
+{
+public:
+  template <class TOut>
+  TOut invoke(TOut (*func)())
+  {
+    return func();
+  }
+};
+
 template <class TIn>
 class ParameterClass_1
 {
 protected:
-  TIn param1;
+public: // FIX: temporarily making these public as we iron out architecture & POC
+    TIn param1;
 
 public:
   template <class TOut>
@@ -16,6 +28,7 @@ template <class TIn1, class TIn2>
 class ParameterClass_2 : public ParameterClass_1<TIn1>
 {
 protected:
+public: // FIX: temporarily making these public as we iron out architecture & POC
   TIn2 param2;
 
 public:
@@ -30,6 +43,7 @@ template <class TIn1, class TIn2, class TIn3>
 class ParameterClass_3 : public ParameterClass_2<TIn1, TIn2>
 {
 protected:
+public: // FIX: temporarily making these public as we iron out architecture & POC
   TIn3 param3;
 
 public:
@@ -52,10 +66,14 @@ template <class TParameters, class TFunc>
 class IPCMessage
 {
 protected:
-  TParameters parameters;
-  TFunc func;
+  const TFunc func;
 
 public:
+  IPCMessage(TFunc func) : func(func) {}
+
+  // FIX: temporarily making these public
+  TParameters parameters;
+
   void invoke()
   {
     parameters.invoke(func);
@@ -70,21 +88,28 @@ public:
 };
 
 
-typedef void (*_test_func1)();
-
-template<class TOut> TOut createIPCMessage(_test_func1);
-//template<class TOut, class TIn> createIPCMessage(void (*func)(), TIn in);
+//template<class TOut> TOut createIPCMessage(_test_func1);
 
 //template<>
-IPCMessage<ParameterClass_1<int>, _test_func1> createIPCMessage(_test_func1 func1)
+IPCMessage<ParameterClass_0, void (*)()> createIPCMessage(void (*func)())
 {
-  return IPCMessage<ParameterClass_1<int>, _test_func1>();
+  IPCMessage<ParameterClass_0, void (*)()> m(func);
+  return m;
 }
 
 template<class TIn>
 IPCMessage<ParameterClass_1<TIn>, void (*)(TIn)> createIPCMessage(void (*func)(TIn))
 {
-  IPCMessage<ParameterClass_1<TIn>, void (*)(TIn)>() m;
+  IPCMessage<ParameterClass_1<TIn>, void (*)(TIn)> m(func);
+  return m;
+  //m.parameters.param1 =
+}
+
+
+template<class TIn1, class TIn2>
+IPCMessage<ParameterClass_2<TIn1, TIn2>, void (*)(TIn1, TIn2)> createIPCMessage(void (*func)(TIn1, TIn2))
+{
+  IPCMessage<ParameterClass_2<TIn1, TIn2>, void (*)(TIn1, TIn2)> m(func);
   return m;
   //m.parameters.param1 =
 }
--- test/TestParameterClass.cpp
+++ test/TestParameterClass.cpp
@@ -14,11 +14,32 @@ void test2_ipc(float f)
   printf("\r\nHello world: %f", f);
 }
 
+void test3_ipc(float f, char* str)
+{
+  printf("\r\nHello world: %f:%s", f, str);
+  printf("\r\n");
+}
+
 SCENARIO( "Low level parameter class tests", "[parameter-class]" )
 {
-  createIPCMessage(&test1_ipc);
-  auto m = createIPCMessage(&test2_ipc);
-  m.invoke();
+  WHEN("Doing 0 parameter")
+  {
+    createIPCMessage(&test1_ipc).invoke();
+  }
+  WHEN("Doing 1 parameter")
+  {
+    auto m = createIPCMessage(&test2_ipc);
+    m.parameters.param1 = 32;
+    m.invoke();
+  }
+  WHEN("Doing 2 parameter")
+  {
+    auto m2 = createIPCMessage(&test3_ipc);
+    m2.parameters.param1 = 7;
+    m2.parameters.param2 = "lucky #7";
+    m2.invoke();
+  }
+
   GIVEN("")
   {
 
