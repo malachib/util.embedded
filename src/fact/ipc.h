b26a1e2a4fc73674b1ca483d9921187a7dddb1cd
Malachi Burke <malachi.burke@gmail.com>
2016-04-07 11:06:08 -0700

    Initial experimentation

--- src/fact/ipc.h
+++ src/fact/ipc.h
@@ -1,14 +1,39 @@
+// NOTE: function references are available, but instance-member references are not
+// (only instance-member pointers)
+// I decided to use function references *when we can* to make consuming code more
+// readable.  For why C++ doesn't have instance-member references:
+//
+// http://stackoverflow.com/questions/21952386/why-doesnt-reference-to-member-exist-in-c
+
+class ParameterClass_0
+{
+public:
+  template <class TOut>
+  TOut invoke(TOut (&func)())
+  {
+    return func();
+  }
+};
+
 template <class TIn>
 class ParameterClass_1
 {
 protected:
-  TIn param1;
+public: // FIX: temporarily making these public as we iron out architecture & POC
+    TIn param1;
 
 public:
   template <class TOut>
-  TOut invoke(TOut (*func)(TIn))
+  TOut invoke(TOut (&func)(TIn))
   {
     return func(param1);
+  }
+
+  // TClass and TIn1 must always match
+  template <class TClass, class TOut>
+  TOut invoke(TOut (TClass::*func)())
+  {
+    return ((*param1).*func)();
   }
 };
 
@@ -16,13 +41,22 @@ template <class TIn1, class TIn2>
 class ParameterClass_2 : public ParameterClass_1<TIn1>
 {
 protected:
+public: // FIX: temporarily making these public as we iron out architecture & POC
   TIn2 param2;
 
 public:
   template <class TOut>
-  TOut invoke(TOut (*func)(TIn1, TIn2))
+  TOut invoke(TOut (&func)(TIn1, TIn2))
   {
     return func(ParameterClass_1<TIn1>::param1, param2);
+  }
+
+
+  // TClass and TIn1 must always match
+  template <class TClass, class TOut>
+  TOut invoke(TOut (TClass::*func)(TIn2))
+  {
+    return ((*ParameterClass_1<TIn1>::param1).*func)(param2);
   }
 };
 
@@ -30,11 +64,12 @@ template <class TIn1, class TIn2, class TIn3>
 class ParameterClass_3 : public ParameterClass_2<TIn1, TIn2>
 {
 protected:
+public: // FIX: temporarily making these public as we iron out architecture & POC
   TIn3 param3;
 
 public:
   template <class TOut>
-  TOut invoke(TOut (*func)(TIn1, TIn2, TIn3))
+  TOut invoke(TOut (&func)(TIn1, TIn2, TIn3))
   {
     return func(ParameterClass_1<TIn1>::param1, ParameterClass_2<TIn1, TIn2>::param2, param3);
   }
@@ -47,17 +82,135 @@ class ParameterReturnClass
   TOut returnValue;
 };
 
+class IInvoker
+{
+public:
+  /* this doesn't work
+  template <class TOut>
+  virtual TOut invoke() = 0; */
+
+  virtual void invoke() = 0;
+};
+
+
+class IPCHelper;
 
 template <class TParameters, class TFunc>
-class IPCMessage
+class IPCMessage : public IInvoker
 {
+  friend IPCHelper;
+
 protected:
-  TParameters parameters;
-  TFunc func;
+  const TFunc func;
 
 public:
-  void invoke()
+  TParameters parameters;
+
+  IPCMessage(TFunc func) : func(func) {}
+
+  virtual void invoke() override
   {
     parameters.invoke(func);
   }
+};
+
+template <class TParameters, class TFunc>
+class IPCMessageMethod : IPCMessage<TParameters, TFunc>
+{
+  void invoke()
+  {
+  }
+};
+
+//template<class TOut> TOut createIPCMessage(_test_func1);
+
+//template<>
+IPCMessage<ParameterClass_0, void (&)()> createIPCMessage(void (&func)())
+{
+  IPCMessage<ParameterClass_0, void (&)()> m(func);
+  return m;
+}
+
+template<class TIn>
+IPCMessage<ParameterClass_1<TIn>, void (&)(TIn)> createIPCMessage(void (&func)(TIn))
+{
+  IPCMessage<ParameterClass_1<TIn>, void (&)(TIn)> m(func);
+  return m;
+  //m.parameters.param1 =
+}
+
+
+template<class TIn1, class TIn2>
+IPCMessage<ParameterClass_2<TIn1, TIn2>, void (&)(TIn1, TIn2)> createIPCMessage(void (&func)(TIn1, TIn2))
+{
+  IPCMessage<ParameterClass_2<TIn1, TIn2>, void (&)(TIn1, TIn2)> m(func);
+  return m;
+  //m.parameters.param1 =
+}
+
+
+template <class T>
+IPCMessage<ParameterClass_1<T*>, void (T::*)()> createIPCMessage(void (T::*func)())
+{
+  IPCMessage<ParameterClass_1<T*>, void (T::*)()> m(func);
+  return m;
+}
+
+
+template <class T, class TIn1>
+IPCMessage<ParameterClass_2<T*, TIn1>, void (T::*)(TIn1)> createIPCMessage(void (T::*func)(TIn1))
+{
+  IPCMessage<ParameterClass_2<T*, TIn1>, void (T::*)(TIn1)> m(func);
+  return m;
+}
+
+class IPCHelper
+{
+public:
+  // ditching template class TFunc trick because:
+  // a) it doesn't gracefully resolve func ref vs func ptr
+  // b) it forces TIn resolution to hang primarily off of create parameters
+  //    vs actual function parameters, and we'd prefer function parameters
+
+/*
+  template <class TFunc, class TIn1>
+  static IPCMessage<ParameterClass_1<TIn1>, TFunc> _create(TFunc func, TIn1 in1)
+  {
+    auto m = createIPCMessage(func);
+    m.parameters.param1 = in1;
+    return m;
+  }*/
+
+
+  //template <class TOut>
+  static IPCMessage<ParameterClass_0, void (&)()> create(void (&func)())
+  {
+    return createIPCMessage(func);
+  }
+
+  template <class TIn1>
+  static IPCMessage<ParameterClass_1<TIn1>, void (&)(TIn1)> create(void (&func)(TIn1), TIn1 in1)
+  {
+    auto m = createIPCMessage(func);
+    m.parameters.param1 = in1;
+    return m;
+  }
+
+  template <class TClass>
+  static IPCMessage<ParameterClass_1<TClass>, void (TClass::*)()> create(void (TClass::*func)(), TClass in1)
+  {
+    auto m = createIPCMessage(func);
+    m.parameters.param1 = in1;
+    return m;
+  }
+
+  template <class TFunc, class TIn1, class TIn2>
+  static IPCMessage<ParameterClass_2<TIn1, TIn2>, TFunc> create(TFunc func, TIn1 in1, TIn2 in2)
+  {
+    auto m = createIPCMessage(func);
+    m.parameters.param1 = in1;
+    m.parameters.param2 = in2;
+    return m;
+  }
+  //IPCMessage
 };
--- test/TestParameterClass.cpp
+++ test/TestParameterClass.cpp
@@ -3,8 +3,86 @@
 #include <fact/ipc.h>
 #include <menu/Properties.h>
 
+
+class TestParameterClass
+{
+  int value = 1;
+
+public:
+  void test()
+  {
+    printf("\r\nFrom parameter class tester: %d", value);
+  }
+
+
+  void test2(int value)
+  {
+    this->value = value;
+
+    printf("\r\nFrom parameter class tester: %d", value);
+  }
+};
+
+void test1_ipc()
+{
+  printf("\r\nHello world");
+}
+
+void test2_ipc(float f)
+{
+  printf("\r\nHello world: %f", f);
+}
+
+void test3_ipc(float f, const char* str)
+{
+  printf("\r\nHello world: %f:%s", f, str);
+  printf("\r\n");
+}
+
 SCENARIO( "Low level parameter class tests", "[parameter-class]" )
 {
+  WHEN("Doing 0 parameter")
+  {
+    createIPCMessage(test1_ipc).invoke();
+  }
+  WHEN("Doing 1 parameter")
+  {
+    auto m = IPCHelper::create(test2_ipc, 32.0F);
+    /*
+    auto m = createIPCMessage(test2_ipc);
+    m.parameters.param1 = 32; */
+    m.invoke();
+  }
+  WHEN("Doing 2 parameter")
+  {
+    auto m2 = createIPCMessage(test3_ipc);
+    m2.parameters.param1 = 7;
+    m2.parameters.param2 = "lucky #7";
+    m2.invoke();
+  }
+  WHEN("Doing 0 parameter, instance method")
+  {
+    TestParameterClass tpc;
+
+    auto m = createIPCMessage(&TestParameterClass::test);
+    m.parameters.param1 = &tpc;
+    m.invoke();
+  }
+  WHEN("Doing 1 parameter, instance method")
+  {
+    TestParameterClass tpc;
+
+    auto m = IPCHelper::create(&TestParameterClass::test2, &tpc, 7);
+    /*auto m = createIPCMessage(&TestParameterClass::test2);
+    m.parameters.param1 = &tpc;
+    m.parameters.param2 = 7;
+    m.invoke(); */
+
+    IInvoker* invoker = &m;
+
+    invoker->invoke();
+  }
+
   GIVEN("")
   {
 
