4cad95d30d9efa82bf3b1eaf78e17794954d1fdc
Malachi Burke <malachi.burke@gmail.com>
2016-04-07 14:37:44 -0700

    Unit test cleanup

--- test/TestParameterClass.cpp
+++ test/TestParameterClass.cpp
@@ -3,6 +3,7 @@
 #include <fact/ipc.h>
 #include <menu/Properties.h>
 
+using namespace FactUtilEmbedded;
 
 class TestParameterClass
 {
@@ -11,50 +12,57 @@ class TestParameterClass
 public:
   void test()
   {
-    printf("\r\nFrom parameter class tester: %d", value);
+    value++;
   }
 
 
   void test2(int value)
   {
     this->value = value;
-
-    printf("\r\nFrom parameter class tester: %d", value);
   }
+
+  int getValue() { return value; }
 };
+
+
+static float ipc_test_value = 0;
+std::string ipc_test_str = "";
 
 void test1_ipc()
 {
-  printf("\r\nHello world");
+  ipc_test_value++;
 }
 
 void test2_ipc(float f)
 {
-  printf("\r\nHello world: %f", f);
+  ipc_test_value = f;
 }
 
 void test3_ipc(float f, const char* str)
 {
-  printf("\r\nHello world: %f:%s", f, str);
-  printf("\r\n");
+  ipc_test_value = f;
+  ipc_test_str = str;
 }
 
 SCENARIO( "Low level parameter class tests", "[parameter-class]" )
 {
   WHEN("Doing 0 parameter")
   {
-    //createIPCMessage(test1_ipc).invoke();
     IPCHelper::create(test1_ipc).invoke();
+    REQUIRE(ipc_test_value == 1);
   }
   WHEN("Doing 1 parameter")
   {
     auto m = IPCHelper::create(test2_ipc, 32.0F);
     m.invoke();
+    REQUIRE(ipc_test_value == 32);
   }
   WHEN("Doing 2 parameter")
   {
     auto m2 = IPCHelper::create(test3_ipc, 7.0F, "lucky #7");
     m2.invoke();
+    REQUIRE(ipc_test_value == 7);
+    REQUIRE(ipc_test_str == "lucky #7");
   }
   WHEN("Doing 0 parameter, instance method")
   {
@@ -62,6 +70,8 @@ SCENARIO( "Low level parameter class tests", "[parameter-class]" )
 
     auto m = IPCHelper::create(&TestParameterClass::test, &tpc);
     m.invoke();
+
+    REQUIRE(tpc.getValue() == 2);
   }
   WHEN("Doing 1 parameter, instance method")
   {
@@ -72,6 +82,8 @@ SCENARIO( "Low level parameter class tests", "[parameter-class]" )
     IInvoker* invoker = &m;
 
     invoker->invoke();
+
+    REQUIRE(tpc.getValue() == 7);
   }
 
   GIVEN("")
