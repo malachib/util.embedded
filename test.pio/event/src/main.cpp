8701812514b9395f5c6caf3878baa1851e223c1c
Malachi Burke <malachi.burke@gmail.com>
2016-04-09 23:03:18 -0700

    More metric gathering

--- test.pio/event/src/main.cpp
+++ test.pio/event/src/main.cpp
@@ -11,6 +11,8 @@ public:
   Event testEvent1;
 } efc;
 
+EventExp1<EventFiringClass*> eventExp1;
+
 
 uint8_t eventResponder_counter = 0;
 
@@ -23,6 +25,7 @@ void eventResponder(EventFiringClass* source)
 void setup() 
 {
   efc.testEvent1 += eventResponder;
+  //eventExp1 += eventResponder;
 }
 
 void loop()
