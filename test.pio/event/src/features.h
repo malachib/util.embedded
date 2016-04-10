c885301abfde6ff57f5ef583b0a4d1076c4d956d
Malachi Burke <malachi.burke@gmail.com>
2016-04-10 15:37:57 -0700

    Further metrics

--- test.pio/event/src/features.h
+++ test.pio/event/src/features.h
@@ -9,8 +9,16 @@
 // depth 1:
 // 4146 - 4114 =  32 code used
 //  211 -  210 =   1 data used
-//#define FEATURE_EVENT_ORIGINAL
+#define FEATURE_EVENT_ORIGINAL
 
+
+// FEATURE_EVENT_VA:
+// depth 0:
+// 4186 - 3894 = 292 code used
+//  210 -  148 =  62 data used
+// depth 1:
+// 4234 - 4186 =  48 code used
+//  211 -  210 =   1 data used
 //#define FEATURE_EVENT_VA
 
 // FEATURE_EVENT_TEMPLATE1:
@@ -22,14 +30,14 @@
 //  211 -  210 =   1 data used
 //#define FEATURE_EVENT_TEMPLATE1
 
-// FEATURE_EVENT_TEMPLATE1:
+// FEATURE_EVENT_TEMPLATE2:
 // depth 0:
 // 4162 - 3894 = 268 code used
 //  210 -  148 =  62 data used
 // depth 1:
 // 4208 - 4162 =  46 code used
 //  211 -  210 =   1 data used
-#define FEATURE_EVENT_TEMPLATE2
+//#define FEATURE_EVENT_TEMPLATE2
 
 
 #if defined(FEATURE_EVENT_ORIGINAL) || defined(FEATURE_EVENT_VA) || defined(FEATURE_EVENT_TEMPLATE1) || defined(FEATURE_EVENT_TEMPLATE2)
