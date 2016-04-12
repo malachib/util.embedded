f0c8d1ff305711ac3ffb3446f2a46288cfcf708a
Malachi Burke <malachi.burke@gmail.com>
2016-04-12 02:23:32 -0700

    Churn + extra commenting

--- src/fact/hardware/watchdog_avr.h
+++ src/fact/hardware/watchdog_avr.h
@@ -8,7 +8,7 @@ namespace FactUtilEmbedded
   class WatchdogControl
   {
     static uint8_t buildPrescalar(const uint8_t wdto);
-    
+
   public:
     struct Control
     {
@@ -16,12 +16,12 @@ namespace FactUtilEmbedded
       {
         enabled = true;
       }
-      
+
       void off()
       {
         enabled = false;
       }
-      
+
       bool isEnabled() const { return enabled; }
       bool operator()() const { return enabled; }
 
@@ -31,16 +31,18 @@ namespace FactUtilEmbedded
 
     static Control isr;
     static Control systemReset;
-    
-    // "Reset the watchdog timer. When the watchdog timer is enabled, 
-    // a call to this instruction is required before the timer expires, 
+
+    // "Reset the watchdog timer. When the watchdog timer is enabled,
+    // a call to this instruction is required before the timer expires,
     // otherwise a watchdog-initiated device reset will occur."
     // - http://www.atmel.com/webdoc/AVRLibcReferenceManual/group__avr__watchdog.html
     static void reset() { wdt_reset(); }
     static void disable() { wdt_disable(); }
-    
+
+    // did I just rewrite this?
+    // http://www.atmel.com/webdoc/AVRLibcReferenceManual/group__avr__watchdog_1gaf6cfea2a1b61e2530ea0c4ef8fc572b3.html
     static void enable(const uint8_t wdto);
   };
-  
+
   extern WatchdogControl Watchdog;
 }
