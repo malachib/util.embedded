cfd03d8a4e0888a6aad7330cdc57f8c3e68a77f0
Malachi Burke <malachi.burke@gmail.com>
2017-01-05 16:50:16 -0800

    Building out streambuf
    Not utilized in any way yet

--- src/fact/iostream.h
+++ src/fact/iostream.h
@@ -31,9 +31,64 @@ extern "C"
 
 namespace FactUtilEmbedded { namespace std
 {
-
 typedef uint16_t streamoff;
 typedef uint16_t streamsize;
+
+// TODO: ensure we're creating int_type properly
+typedef int16_t int_type;
+
+//  As per http://tuttlem.github.io/2014/08/18/getting-istream-to-work-off-a-byte-array.html
+//  I won't try to stuff in a uint8_t as a TChar anywhere, although it seems like I could
+// safely make a fully uint8_t version of things
+
+template <class TChar> struct char_traits;
+template <> struct char_traits<char>
+{
+    static int_type to_int_type(char ch) { return ch; }
+    static int_type eof() { return -1; }
+};
+
+template<class TChar, class Traits = char_traits<TChar>>
+class basic_streambuf
+{
+    typedef TChar char_type;
+
+protected:
+    virtual streamsize xsputn(const char_type* s, streamsize count) = 0;
+    virtual streamsize xsgetn(char_type* s, streamsize count) = 0;
+
+    //virtual int_type uflow() = 0;
+
+public:
+    streamsize sputn(const char_type* s, streamsize count)
+    {
+        return xsputn(s, count);
+    }
+
+    int_type sputc(char_type ch)
+    {
+        bool success = xsputn(&ch, sizeof(ch)) == sizeof(ch);
+        return success ? (int_type) ch : Traits::eof();
+    }
+
+    streamsize sgetn(char_type* s, streamsize count)
+    {
+        return xsgetn(s, count);
+    }
+
+    // Don't think I made this one quite right...
+    int_type sbumpc()
+    {
+        char_type ch;
+
+        bool success = xsgetn(&ch, sizeof(ch)) == sizeof(ch);
+
+        return success ? Traits::to_int_type(ch) : Traits::eof();
+    }
+};
+
+
+typedef basic_streambuf<char> streambuf;
 
 //typedef basic_ostream __ostream_type;
 
