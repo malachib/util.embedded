75540e680a558271dd887192643a8f6e25472420
Malachi Burke <malachi.burke@gmail.com>
2017-01-05 17:11:09 -0800

    Proof of concept testing

--- test/TestIOStream.cpp
+++ test/TestIOStream.cpp
@@ -8,6 +8,23 @@
 
 using namespace FactUtilEmbedded::std;
 
+struct Special
+{
+    const char* value;
+};
+
+template <class TOStream>
+inline TOStream& operator <<(TOStream& out, Special& arg)
+{
+    auto& retVal = out.write(arg.value, strlen(arg.value));
+    // PRESUMABLY: C++ std iostreams use basic_ostream specializations to get around
+    // the issue experienced here with return retVal (I'm using actual
+    // inheritance from basic_ostream, not specialization)
+    //return retVal;
+    return out;
+}
+
+
 SCENARIO( "IOStream tests", "[iostream]" )
 {
   WHEN("Printing a normal string")
@@ -22,5 +39,13 @@ SCENARIO( "IOStream tests", "[iostream]" )
   WHEN("Printing raw characters")
   {
       cout << '\n' << '\r';
+  }
+  WHEN("Testing special overload")
+  {
+      Special special;
+
+      special.value = "Special value output\r\n";
+
+      cout  << special;
   }
 }
