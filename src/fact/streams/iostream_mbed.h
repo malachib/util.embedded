3c5f741f3c85fe286533dae1e475bcf598bb30d9
Malachi Burke <malachi.burke@gmail.com>
2017-02-26 19:46:56 -0800

    Got mbed compiling at a minimal level
    * Seems to be using wrong GCC (4.8 at time of writing, pretty sure mbed wants 4.9)
    * ios-shim code needs more wiring up before it actually functions

--- src/fact/streams/iostream_mbed.h
+++ src/fact/streams/iostream_mbed.h
@@ -1,14 +1,67 @@
 #pragma once
 
+//#include "mbed.h"
+
 extern "C"
 {
-    #include <unistd.h>
+    //#include <unistd.h>
     #include <stdio.h>
 }
 
-#error "MBED OS not yet supported, but coming"
+#include "Stream.h"
+
+//#error "MBED OS not yet supported, but coming"
 
 
 namespace FactUtilEmbedded { namespace std
 {
+using namespace mbed;
+
+class istream : public basic_istream<char>
+{
+    Stream* input;
+    FileLike& getFile() { return *input; }
+
+public:
+    istream& read(char* s, streamsize n)
+    {
+        FileLike& in = getFile();
+
+        in.read(s, n);
+
+        return *this;
+    }
+
+    int get()
+    {
+        return input->getc();
+    }
+
+    bool eof() { return false; }
+};
+
+
+class ostream : public basic_ostream<char>
+{
+    Stream* output;
+    FileLike& getFile() { return *output; }
+
+public:
+    __ostream_type& write(const char* s, streamsize n) override
+    {
+        FileLike& file = getFile();
+
+        file.write(s, n);
+
+        return *this;
+    }
+
+    __ostream_type& put(char ch) override
+    {
+        output->putc(ch);
+
+        return *this;
+    }
+};
+
 } }
--- test.pio/ios-shim/src/main.cpp
+++ test.pio/ios-shim/src/main.cpp
@@ -3,5 +3,12 @@
 
 int main()
 {
+    static int counter = 0;
+
+    for(;;)
+    {
+        printf("Waiting...%d\r\n", counter++);
+        wait(1.0);
+    }
     return 0;
 }
