4799324544d1c7faa8ce0f3af40ac88e60da5878
Malachi Burke <malachi.burke@gmail.com>
2017-05-29 20:57:12 -0700

    Adding unused and nascent locate code to eventually fuel things such as:
    * whitespace detection
    * locale information for date/time
    * money formatting

---
+++ src/fact/locale.h
@@ -0,0 +1,43 @@
+#pragma once
+
+namespace FactUtilEmbedded { namespace std { namespace experimental {
+
+struct locale
+{
+
+};
+
+struct ctype_base
+{
+    typedef uint8_t mask;
+
+    static constexpr max space = 0x01;
+};
+
+
+template <class TChar>
+class ctype : public ctype_base
+{
+public:
+    bool is(mask m, TChar ch) const { return false; }
+    const TChar* is(const TChar* low, const TChar* high, mask* vec) const { return nullptr; }
+};
+
+// specialization, deviating from standard in that locale is compile-time
+// instead of runtime
+template <>
+class ctype<char>
+{
+
+};
+
+template <class TFacet>
+const TFacet& use_facet(const locale& loc);
+
+template <class TChar>
+inline bool isspace(TChar ch, const locale& loc)
+{
+    return use_facet<ctype<TChar>>(loc).is(ctype_base::space, ch);
+}
+
+}}}
